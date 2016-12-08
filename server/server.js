2159a735a5f2f4ff06f5e15f0e6218cf2f7521a9
Ian Hamilton <ian.hamilton.it@gmail.com>
2016-12-08 13:39:34 -0800

    added some json formatting for component output

--- server/server.js
+++ server/server.js
@@ -12,10 +12,19 @@
 
 express = require('express');
 var app = express();
+bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 
 var port = process.env.PORT || 8888;
 
 app.use(express.static(__dirname + '/../public'));
+
+app.route('/api/customer')
+    .post(function(req, rep) {
+        var customer = req.body
+        rep.status(200).send("success");
+    });
 
 app.listen(port);
 console.log('Magic happens on port ' + port + ' ' + __dirname + '/../public');
